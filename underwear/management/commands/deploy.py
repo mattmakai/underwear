313adf27ed0eee7b08765601bca4d7dbe4282927
Matthew Makai <makaimc@gmail.com>
2013-12-19 11:32:35 -0500

    updating script to include ansible playbook execution hook

--- underwear/management/commands/deploy.py
+++ underwear/management/commands/deploy.py
@@ -3,11 +3,12 @@ from django.core.management.base import BaseCommand, CommandError
 from underwear.run_underwear import deploy
 
 class Command(BaseCommand):
-    args = '/etc/ansible/hosts'
+    args = '[hosts file location]'
     help = 'Deploy to a remote server.'
 
     def handle(self, *args, **options):
         if len(args) < 1:
             print "Usage: manage.py deploy [hosts file location]\n"
-        deploy(['django-stack.yml', '-i', args[0]])
-        self.stdout.write('App successfully deployed to server.')
+        else:
+            deploy(['django-stack.yml', '-i', args[0]])
+            self.stdout.write('App successfully deployed to server.')
--- underwear/run_underwear.py
+++ underwear/run_underwear.py
@@ -6,15 +6,37 @@
 # See https://groups.google.com/forum/#!topic/ansible-project/V1PoNJcXV_w
 # for further details. 
 # Original file I worked from: 
-# https://groups.google.com/forum/#!topic/ansible-project/V1PoNJcXV_w
+# https://github.com/ansible/ansible/blob/devel/bin/ansible
 #
 ##
 
-import sys
+import sys, os
 
 import ansible.playbook
 import ansible.constants as C
 from ansible import errors, callbacks, utils
+from ansible.color import ANSIBLE_COLOR, stringc
+from ansible.callbacks import display
+
+
+def colorize(lead, num, color):
+    """ Print 'lead' = 'num' in 'color' """
+    if num != 0 and ANSIBLE_COLOR and color is not None:
+        return "%s%s%-15s" % (stringc(lead, color), stringc("=", color), stringc(str(num), color))
+    else:
+        return "%s=%-4s" % (lead, str(num))
+
+
+def hostcolor(host, stats, color=True):
+    if ANSIBLE_COLOR and color:
+        if stats['failures'] != 0 or stats['unreachable'] != 0:
+            return "%-37s" % stringc(host, 'red')
+        elif stats['changed'] != 0:
+            return "%-37s" % stringc(host, 'yellow')
+        else:
+            return "%-37s" % stringc(host, 'green')
+    return "%-26s" % host
+
 
 def deploy(args):
     # create parser for CLI options
@@ -38,5 +60,101 @@ def deploy(args):
     if len(inventory.list_hosts()) == 0:
         raise errors.AnsibleError("provided hosts list is empty")
 
-    print "application deployment hook worked"
+    sshpass = None
+    sudopass = None
+    options.ask_pass = options.ask_pass or C.DEFAULT_ASK_PASS
+    if options.connection == "local":
+        options.ask_pass = False
+    options.ask_sudo_pass = options.ask_sudo_pass or C.DEFAULT_ASK_SUDO_PASS
+    (sshpass, sudopass) = utils.ask_passwords(ask_pass=options.ask_pass, 
+        ask_sudo_pass=options.ask_sudo_pass)
+    options.sudo_user = options.sudo_user or C.DEFAULT_SUDO_USER
+
+    playbook = args[0]
+    inventory.set_playbook_basedir(os.path.dirname(playbook))
+    stats = callbacks.AggregateStats()
+    playbook_cb = callbacks.PlaybookCallbacks(verbose=utils.VERBOSITY)
+    runner_cb = callbacks.PlaybookRunnerCallbacks(stats, 
+        verbose=utils.VERBOSITY)
+
+    pb = ansible.playbook.PlayBook(
+        playbook=playbook,
+        module_path=options.module_path,
+        inventory=inventory,
+        forks=options.forks,
+        remote_user=options.remote_user,
+        remote_pass=sshpass,
+        callbacks=playbook_cb,
+        runner_callbacks=runner_cb,
+        stats=stats,
+        timeout=options.timeout,
+        transport=options.connection,
+        sudo=options.sudo,
+        sudo_user=options.sudo_user,
+        sudo_pass=sudopass,
+        extra_vars=extra_vars,
+        private_key_file=options.private_key_file,
+        only_tags=only_tags,
+        skip_tags=skip_tags,
+        check=options.check,
+        diff=options.diff
+    )
+
+    failed_hosts = []
+    unreachable_hosts = []
+
+    try:
+
+        pb.run()
+
+        hosts = sorted(pb.stats.processed.keys())
+        display(callbacks.banner("PLAY RECAP"))
+        playbook_cb.on_stats(pb.stats)
+
+        for h in hosts:
+            t = pb.stats.summarize(h)
+            if t['failures'] > 0:
+                failed_hosts.append(h)
+            if t['unreachable'] > 0:
+                unreachable_hosts.append(h)
+
+        retries = failed_hosts + unreachable_hosts
+
+        if len(retries) > 0:
+            filename = pb.generate_retry_inventory(retries)
+            if filename:
+                display("           to retry, use: --limit @%s\n" % filename)
+
+        for h in hosts:
+            t = pb.stats.summarize(h)
+
+            display("%s : %s %s %s %s" % (
+                hostcolor(h, t),
+                colorize('ok', t['ok'], 'green'),
+                colorize('changed', t['changed'], 'yellow'),
+                colorize('unreachable', t['unreachable'], 'red'),
+                colorize('failed', t['failures'], 'red')),
+                screen_only=True
+            )
+
+            display("%s : %s %s %s %s" % (
+                hostcolor(h, t, False),
+                colorize('ok', t['ok'], None),
+                colorize('changed', t['changed'], None),
+                colorize('unreachable', t['unreachable'], None),
+                colorize('failed', t['failures'], None)),
+                log_only=True
+            )
+        print ""
+        if len(failed_hosts) > 0:
+            return 2
+        if len(unreachable_hosts) > 0:
+            return 3
+
+    except errors.AnsibleError, e:
+        display("ERROR: %s" % e, color='red')
+        return 1
+
+    return 0
+
 
