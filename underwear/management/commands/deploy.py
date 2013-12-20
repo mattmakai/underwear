c72db3bdea4269431d8f486edba6cc229fb641ee
Matthew Makai <makaimc@gmail.com>
2013-12-20 10:23:21 -0500

    working on integrating ansible commands with django command.

--- underwear/management/commands/deploy.py
+++ underwear/management/commands/deploy.py
@@ -10,5 +10,5 @@ class Command(BaseCommand):
         if len(args) < 1:
             print "Usage: manage.py deploy [hosts file location]\n"
         else:
-            deploy(['django-stack.yml', '-i', args[0]])
+            deploy(['../../django-stack.yml', '-i', args[0]])
             self.stdout.write('App successfully deployed to server.')
