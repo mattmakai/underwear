0fe086deb2130921596be0733c16437e284e8a90
Matthew Makai <makaimc@gmail.com>
2013-12-21 11:39:08 -0500

    adding private key to options for django command (needs a default)

--- underwear/management/commands/deploy.py
+++ underwear/management/commands/deploy.py
@@ -3,12 +3,14 @@ from django.core.management.base import BaseCommand, CommandError
 from underwear.run_underwear import deploy
 
 class Command(BaseCommand):
-    args = '[hosts file location]'
+    args = '[hosts file location] [private key path and filename]'
     help = 'Deploy to a remote server.'
 
     def handle(self, *args, **options):
-        if len(args) < 1:
-            print "Usage: manage.py deploy [hosts file location]\n"
+        if len(args) < 2:
+            print "Usage: manage.py deploy [hosts file location] " + \
+                "[private key path and filename]\n"
         else:
-            deploy(['django-stack.yml', '-i', args[0]])
+            deploy(['django-stack.yml', '-i', args[0], '-K', '-u', 
+                'deployer', '--private-key=%s' % args[1]])
             self.stdout.write('App successfully deployed to server.')
