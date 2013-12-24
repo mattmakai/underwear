794bf2e0977af283ce2f070f336aeca8ee9c3136
Matthew Makai <makaimc@gmail.com>
2013-12-24 10:31:50 -0600

    working on passing external file as extra vars option

--- underwear/management/commands/deploy.py
+++ underwear/management/commands/deploy.py
@@ -14,5 +14,5 @@ class Command(BaseCommand):
         else:
             deploy(['django-stack.yml', '-i', args[0], '-K', '-u', 
                 'deployer', '--private-key=%s' % args[1], 
-                '--extra-vars', '@%s' % args[2]])
+                '--extra-vars', '"@%s"' % args[2]])
             self.stdout.write('App successfully deployed to server.')
